f424a48fa5d21a266f29e25e71d363b31aababe2
Ulrika Karlsson <ulrika.karlsson@yahoo.se>
2020-11-05 21:13:18 +0100

    Add project files.

---
+++ Car_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_1
+{
+    class Program
+    {
+        class Car
+        {
+            private string carBrand, yearModel;
+            private int numOfMil = 0;
+            public Car (string C, string Y, int M)
+            {
+                carBrand = C;
+                yearModel = Y;
+                numOfMil = M;
+            }
+            public int NumOfMil()
+            {
+                return numOfMil;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Car C = new Car("Lada", "1990", 137682);
+            Console.WriteLine("Antal mil = {0}", C.NumOfMil());
+        }
+    }
+}
